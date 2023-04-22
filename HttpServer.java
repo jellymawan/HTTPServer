ebfc6816913ef6beceb7627d539c337a24563d80
jellymawan <62910852+jellymawan@users.noreply.github.com>
2023-04-21 23:57:08 -0700

    POST is buggy, but working
    As long as its not writing to the browser, it works fine.

--- HttpServer.java
+++ HttpServer.java
@@ -4,9 +4,10 @@ import java.util.HashMap;
 
 public class HttpServer {
 
+    static String requestURL;
+
     public static void main(String[] args) throws IOException {
         int port = 80;
-        String requestURL = "";
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Server running on port: " + port);
 
@@ -25,13 +26,10 @@ public class HttpServer {
 
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-
                 String line = in.readLine();
+                requestURL = line.substring(line.indexOf("/") + 1, line.indexOf("HTTP") - 1);
+
                 if(line.contains("GET")) {
-
-                    requestURL = line.substring(line.indexOf("/") + 1, line.indexOf("HTTP") - 1);
-                    System.out.println(requestURL);
-
                     if(path.containsKey("/" + requestURL)) {
                         File f = new File("./" + requestURL);
                         byte[] bytes = new byte[(int) f.length()];
@@ -53,7 +51,7 @@ public class HttpServer {
                         fis.close();
                         bufInputStream.close();
                         os.close();
-                    } else {
+                    }else {
 //                    String http404Response = "HTTP/1.1 404 Not Found\r\n\r\n" + "<!DOCTYPE html>\n" +
 //                            "<html>\n" +
 //                            "\n" +
@@ -82,13 +80,45 @@ public class HttpServer {
 
                         clientOutput.flush();
                     }
+                } else if (line.contains("POST")) {
+                    String requestBody = "";
+                    if (path.get("/" + requestURL).equals("text/plain")) { //can only use POST on text/plain types
+                        while(!line.isEmpty()) { //parses through until it reaches an empty line
+                            //System.out.println(line);
+                            line = in.readLine();
+                        }
+                        requestBody = in.readLine(); //after the empty line, the body of the request
+                        //System.out.println("Word to be appended: " + requestBody);
+                        File f = new File("./" + requestURL);
+                        //System.out.println("Before append, file length: " + f.length());
+                        FileWriter fw = new FileWriter(f, true);
+                        BufferedWriter br = new BufferedWriter(fw);
+                        br.write(requestBody);
+
+                        br.close();
+                        fw.close();
+
+                        //System.out.println("After append, file length: " + f.length());
+
+                        FileInputStream fis = new FileInputStream(requestURL);
+                        BufferedInputStream bufInputStream = new BufferedInputStream(fis);
+                        byte[] bytes = new byte[(int) f.length()];
+                        bufInputStream.read(bytes);
+
+                        //OutputStream os = clientSocket.getOutputStream();
+                        //os.write("HTTP/1.1 200 OK".getBytes());
+                        //os.write("\r\n\r\n".getBytes()); //Currently, this needs to be commented out. After sending the curl, the client waits for a response, and so does the server. I need to exit out of the client
+                        //in order for the server to process the request.
+                        //os.write(bytes);
+                        //os.write("\r\n\r\n".getBytes());
+
+                        //os.flush();
+                        fis.close();
+                        //fis.close();
+                        bufInputStream.close();
+                        //os.close();
+                    }
                 }
-//                } else if(line.contains("POST")) {
-//                }
-
-
-
-
 //                while(!line.isEmpty()) {
 //                    System.out.println(line);
 //
